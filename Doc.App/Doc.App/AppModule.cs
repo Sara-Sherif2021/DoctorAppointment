08418b3bb363c174f82235a1b410edaeeb1e229d
Sara.Sherif <Sara.Sherif2021@outlook.com>
2025-01-28 05:27:17 +0200

    Add Main Project

---
+++ Doc.App/Doc.App/AppModule.cs
@@ -0,0 +1,144 @@
+﻿using Doc.App.Helpers;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.OpenApi.Models;
+using Volo.Abp;
+using Volo.Abp.AspNetCore.Mvc;
+using Volo.Abp.AspNetCore.Mvc.UI.Theme.Shared;
+using Volo.Abp.AspNetCore.Serilog;
+using Volo.Abp.Autofac;
+using Volo.Abp.AutoMapper;
+using Volo.Abp.Emailing;
+using Volo.Abp.Modularity;
+using Volo.Abp.Studio.Client.AspNetCore;
+using Volo.Abp.Swashbuckle;
+
+namespace Doc.App;
+
+[DependsOn(
+    // ABP Framework packages
+    typeof(AbpAutofacModule),
+    typeof(AbpAutoMapperModule),
+    typeof(AbpSwashbuckleModule),
+    typeof(AbpAspNetCoreSerilogModule),
+    typeof(AbpStudioClientAspNetCoreModule)
+)]
+public class AppModule : AbpModule
+{
+    /* Single point to enable/disable multi-tenancy */
+    private const bool IsMultiTenant = false;
+
+    public override void PreConfigureServices(ServiceConfigurationContext context)
+    {
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+        var configuration = context.Services.GetConfiguration();
+
+        PreConfigure<OpenIddictBuilder>(builder =>
+        {
+            builder.AddValidation(options =>
+            {
+                options.AddAudiences("App");
+                options.UseLocalServer();
+                options.UseAspNetCore();
+            });
+        });
+
+        AppGlobalFeatureConfigurator.Configure();
+        AppModuleExtensionConfigurator.Configure();
+    }
+
+    public override void ConfigureServices(ServiceConfigurationContext context)
+    {
+        var hostingEnvironment = context.Services.GetHostingEnvironment();
+        var configuration = context.Services.GetConfiguration();
+
+        if (!hostingEnvironment.IsProduction())
+        {
+            Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+            Microsoft.IdentityModel.Logging.IdentityModelEventSource.LogCompleteSecurityArtifact = true;
+        }
+
+        if (hostingEnvironment.IsDevelopment())
+        {
+            context.Services.Replace(ServiceDescriptor.Singleton<IEmailSender, NullEmailSender>());
+        }
+
+        ConfigureAutoMapper(context);
+        ConfigureSwagger(context.Services, configuration);
+        ConfigureAutoApiControllers();
+    }
+    
+    
+
+    private void ConfigureAutoApiControllers()
+    {
+        Configure<AbpAspNetCoreMvcOptions>(options =>
+        {
+            options.ConventionalControllers.Create(typeof(AppModule).Assembly);
+        });
+    }
+
+    private void ConfigureSwagger(IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAbpSwaggerGen(
+            options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "App API", Version = "v1" });
+                options.DocInclusionPredicate((docName, description) => true);
+                options.CustomSchemaIds(type => type.FullName);
+                options.DocumentFilter<CustomSwaggerFilter>();
+            });
+    }
+
+    private void ConfigureAutoMapper(ServiceConfigurationContext context)
+    {
+        context.Services.AddAutoMapperObjectMapper<AppModule>();
+        Configure<AbpAutoMapperOptions>(options =>
+        {
+            /* Uncomment `validate: true` if you want to enable the Configuration Validation feature.
+             * See AutoMapper's documentation to learn what it is:
+             * https://docs.automapper.org/en/stable/Configuration-validation.html
+             */
+            options.AddMaps<AppModule>(/* validate: true */);
+        });
+    }
+
+ 
+
+    public override void OnApplicationInitialization(ApplicationInitializationContext context)
+    {
+        var app = context.GetApplicationBuilder();
+        var env = context.GetEnvironment();
+
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        app.UseAbpRequestLocalization();
+
+        if (!env.IsDevelopment())
+        {
+            app.UseErrorPage();
+        }
+
+        app.UseCorrelationId();
+        app.UseStaticFiles();
+        app.UseAbpStudioLink();
+        app.UseRouting();
+        app.UseAbpSecurityHeaders();
+        app.UseCors();
+
+        app.UseUnitOfWork();
+        app.UseDynamicClaims();
+
+        app.UseSwagger();
+        app.UseAbpSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "App API");
+        });
+
+        app.UseAuditing();
+        app.UseAbpSerilogEnrichers();
+        app.UseConfiguredEndpoints();
+    }
+}
