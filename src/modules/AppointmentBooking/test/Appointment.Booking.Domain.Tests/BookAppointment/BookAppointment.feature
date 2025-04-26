84bb59aace59e61fa567dc6bd54af360f1d4abd9
Sara.Sherif <Sara.Sherif2021@outlook.com>
2025-04-26 14:02:56 +0300

    add book appointment sample unit test (BDD)

---
+++ src/modules/AppointmentBooking/test/Appointment.Booking.Domain.Tests/BookAppointment/BookAppointment.feature
@@ -0,0 +1,12 @@
+﻿Feature: Book Appointment
+As a patient, I want to be able to book an appointment in a free slot
+
+@tag1
+Scenario: Book Appointment in free slot
+	Given The slot with slotId "3FA85F64-5717-4562-B3FC-2C963F66AFA5" is a free slot
+	When when the patient try to book this slot using below data
+		| id | slotId | patientId | patientName | patientEmail | reservedAt |
+		| 3FA85F64-5717-4562-B3FC-2C963F66AFA4 | 3FA85F64-5717-4562-B3FC-2C963F66AFA5 | 3FA85F64-5717-4562-B3FC-2C963F66AFA5 | TestPatient1| TestPatient1@gmail.com | 2025-02-01 |
+	Then The slot with slotId "3FA85F64-5717-4562-B3FC-2C963F66AFA5" will be booked correctly
+
+	
---
+++ src/modules/AppointmentBooking/test/Appointment.Booking.Domain.Tests/BookAppointment/BookAppointmentStepDefinitions.cs
@@ -0,0 +1,57 @@
+using System;
+using Doctor.Availability.Share.Interfaces;
+using NSubstitute;
+using Reqnroll;
+using Volo.Abp.Modularity;
+using Doctor.Availability.Share.Dto;
+using Appointment.Booking.DomainServices;
+using Doctor.Appointment.Share.Services;
+using Volo.Abp.Domain.Repositories;
+using Doctor.Appointment.Share.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Shouldly;
+using NSubstitute.ReturnsExtensions;
+
+namespace Appointment.Booking.BookAppointment
+{
+    [Binding]
+    public class BookAppointmentStepDefinitions
+   
+    {
+        private ISlotAppointmentManager _slotAppointmentManager;
+        private ISlotIntegration _slotIntegration;
+        private INotificationService _notificationService { get; set; }
+        private IRepository<Entities.Appointment> _appointmentRepository { get; set; }
+        private Entities.Appointment _createdAppointment { get; set; }
+        public BookAppointmentStepDefinitions()
+        {
+            _appointmentRepository = Substitute.For<IRepository<Entities.Appointment>>();
+            _notificationService = Substitute.For<INotificationService>();
+            _notificationService.SendEmail(Arg.Any<List<EmailNotificationDto>>()).Returns(Task.CompletedTask);
+
+        }
+
+        [Given("The slot with slotId {string} is a free slot")]
+        public void GivenTheSlotWithSlotIdIsAFreeSlot(string slotId)
+        {
+            _slotIntegration = Substitute.For<ISlotIntegration>();
+            _slotIntegration.GetAvailableSlotById(new Guid(slotId)).Returns(new AvailableSlotResultDto { Id = new Guid(slotId) });
+
+        }
+        [When("when the patient try to book this slot using below data")]
+        public async Task WhenWhenThePatientTryToBookThisSlotUsingBelowData(DataTable dataTable)
+        {
+            _slotAppointmentManager = new SlotAppointmentManager(_slotIntegration, _notificationService, _appointmentRepository);
+            var appointmentTable = dataTable.CreateInstance<(Guid id, Guid slotId, Guid patientId, string patientName, string patientEmail, DateTime reservedAt)>();
+            _createdAppointment = await _slotAppointmentManager.CreateAppointment(appointmentTable.id, appointmentTable.slotId, appointmentTable.patientId, appointmentTable.patientName, appointmentTable.patientEmail, appointmentTable.reservedAt);
+        }
+
+        [Then("The slot with slotId {string} will be booked correctly")]
+        public void ThenTheSlotWithSlotIdWillBeBookedCorrectly(string slotId)
+        {
+            _createdAppointment.ShouldNotBeNull();
+            _createdAppointment.SlotId.ShouldBe(new Guid(slotId));
+        }
+    }
+}
